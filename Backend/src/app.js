b87ea9551297986ac581fafbc655aeaced209df1
Rahulmaharaj1 <rahulmaharaj9958@gmail.com>
2026-06-13 09:04:37 +0530

    Connected frontend with backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,12 +7,13 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({
+    origin:[
+        "http://localhost:5173",
+        "https://rahulmaharaj1.github.io"
+    ],
+    credentials:true
+}));
 
 /* require routes */
 const authRouter = require("./routes/auth.routes");
