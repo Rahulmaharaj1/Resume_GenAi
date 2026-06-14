a1382f4697c5d387cd019fb61761b2865ad04ecc
Rahulmaharaj1 <rahulmaharaj9958@gmail.com>
2026-06-14 09:53:09 +0530

    .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,34 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./app.routes.jsx";
-import { AuthProvider } from "./features/auth/auth.context.jsx";
-import { InterviewProvider } from "./features/interview/interview.context.jsx";
+// import { RouterProvider } from "react-router-dom";
+// import { router } from "./app.routes.jsx";
+// import { AuthProvider } from "./features/auth/auth.context.jsx";
+// import { InterviewProvider } from "./features/interview/interview.context.jsx";
 
-function App() {
-  return (
-    <AuthProvider>
-      <InterviewProvider>
-        <RouterProvider router={router} />
-      </InterviewProvider>
-    </AuthProvider>
-  );
+// function App() {
+//   return (
+//     <AuthProvider>
+//       <InterviewProvider>
+//         <RouterProvider router={router} />
+//       </InterviewProvider>
+//     </AuthProvider>
+//   );
+// }
+
+// export default App;
+
+function App(){
+
+return (
+
+<div>
+
+<h1>
+Resume Gen AI Working 🚀
+</h1>
+
+</div>
+
+)
+
 }
 
 export default App;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./style.scss";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/Resume_GenAi">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+  <App />
 );
